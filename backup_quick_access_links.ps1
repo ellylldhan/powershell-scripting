e7e0a7588c0ff9aa611a0bf3ab61191fb2d5a766
reno-slf <rinux.rinux@gmail.com>
2024-12-24 09:44:31 +0100

    ajout brouillon backup liens Acces rapide Quick access

---
+++ backup_quick_access_links.ps1
@@ -0,0 +1,3 @@
+﻿#robocopy \\self-fix-099\c$\Users\franck.p\AppData\Roaming\Microsoft\Windows\Recent\AutomaticDestinations "C:\TempQA"
+
+robocopy "C:\TempQA" %appdata%\microsoft\windows\recent\automaticdestinations *.automaticDestinations-ms
